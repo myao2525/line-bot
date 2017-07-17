30a9af787b40efd28724bbbe8e1ed8f84cbb0ec2
Mao Yamamura <myamamura@4cast.co.jp>
2017-07-17 13:41:48 +0900

    反応追加

--- app.rb
+++ app.rb
@@ -70,6 +70,7 @@ post '/callback' do
           }
         end
         client.reply_message(event['replyToken'], message)
+      end
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
